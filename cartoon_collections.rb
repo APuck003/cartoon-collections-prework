405e7cca21a140816f3eafb3648fc39a7ba50a91
Austin Puckett <puckett_austin@yahoo.com>
2018-10-05 02:46:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,13 +4,16 @@ def roll_call_dwarves(array)
   end
 end
 
+
 def summon_captain_planet# code an argument here
   
 end
 
+
 def long_planeteer_calls# code an argument here
   
 end
+
 
 def find_the_cheese# code an argument here
   
